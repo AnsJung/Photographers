dea059564eb619f4376a5fc8c93ae616f1d01d29
이정현 <cw03125@naver.com>
2026-08-03 18:01:57 +0900

    Initial Commit

---
+++ PhotoGraphers/PhotoGraphersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGraphersApp.swift
+//  PhotoGraphers
+//
+//  Created by 이정현 on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGraphersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
